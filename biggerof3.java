66a25f8656e3428b2781c4edbdae43e2a314efc7
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-27 21:16:26 +0530

    Create biggerof3.java

---
+++ biggerof3.java
@@ -0,0 +1,15 @@
+package startproject;
+
+public class bigof3 {
+	public static void main(String[] args) {
+		int a=45,b=67,c=76;
+		if(a>b && a>c) {
+			System.out.print("a is bigger");
+		}else if (b>a && b>c) {
+			System.out.print("b is bigger");
+		}else {
+			System.out.print("c is bigger");
+		}
+		}
+	}
+
