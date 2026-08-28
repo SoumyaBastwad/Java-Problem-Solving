93d12fe05e5848385d69c7d710ca07e4664f3dc0
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-28 21:22:49 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -6,17 +6,4 @@ public class Palindrome {
 		
          Scanner sc =new Scanner(System.in);
          System.out.print("Enter the string:");
-         String str =sc.nextLine();
-         String reverse ="";
-         for(int i=str.length()-1;i>=0;i--) {
-        	 reverse = reverse  + str.charAt(i);
-         }
-         System.out.println("Reverce string is:" + reverse);
-         if (str.equals(reverse)) {
-     		System.out.print("palindrome");
-     	}else {
-     		System.out.print("not palindrome");
-     	}
-	}
-	
-}
+         String str =sc.nextLine
