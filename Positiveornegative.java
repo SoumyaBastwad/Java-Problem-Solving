c497e9d2262b52c4878877b73a6104dd6f08959f
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-28 21:23:32 +0530

    Update Positiveornegative.java

--- Positiveornegative.java
+++ Positiveornegative.java
@@ -4,12 +4,4 @@ public class Positiveornegative {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("Enter a number");
-		int n=sc.nextInt();
-		if (n>0) {
-			System.out.print("The given number is positive number");
-		}else {
-			System.out.print("The given number is negative number");
-		}
-	}
-
-}
+		
