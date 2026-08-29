d38c2884e8bf82456070d2c68dcab790ad1072d4
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-29 18:23:34 +0530

    Update Bigof3.java

--- Bigof3.java
+++ Bigof3.java
@@ -4,11 +4,4 @@ public class Bigof3 {
 	public static void main(String[] args) {
 		int a=45,b=67,c=76;
 		if(a>b && a>c) {
-			System.out.print("a is bigger");
-		}else if (b>a && b>c) {
-			System.out.print("b is bigger");
-		}else {
-			System.out.print("c is bigger");
-		}
-		}
-	}
+		
