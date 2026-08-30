96a866ce44671254310a28a58ab677a7cfa4737b
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-30 22:48:39 +0530

    Update Pattern8.java

--- Pattern8.java
+++ Pattern8.java
@@ -1,1 +1,17 @@
+package Demo;
+
+public class Pattern8 {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for (int j=1;j<=5;j++) {
+				if(i*j < 10) {
+					System.out.print("0");
+				}
+				System.out.print(i*j + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
 
