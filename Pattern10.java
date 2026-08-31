69875889ddca72898fb7e55b3fbccfa5b2de1e15
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-31 22:41:57 +0530

    Update Pattern10.java

--- Pattern10.java
+++ Pattern10.java
@@ -1,1 +1,18 @@
+package Demo;
 
+public class Pattern10 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) 
+		{
+		  for(int j=1;j<=i;j++) 
+		  {
+			System.out.print("* ");
+		  }
+		  System.out.println();
+	   }
+	    
+
+	}
+
+}
