a70abeea8a916c574df82a7fce8115d86b931aea
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-01 22:39:55 +0530

    Update TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,1 +1,14 @@
+package Demo;
+import java.util.Scanner;
+public class TemperatureConverter {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the temperature in celsius: ");
+		int c =sc.nextInt();
+		float fahreheit = (c*9.0f/5)+32;
+	       System.out.println("in fahreheit: " +fahreheit);
+	}
+
+}
 
