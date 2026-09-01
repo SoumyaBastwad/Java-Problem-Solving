42588ace1b139a18c9f806ba9b485318c5feac1d
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-01 22:30:16 +0530

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,1 +1,18 @@
+package Demo;
+import java.util.Scanner;
+
+public class Square {
+
+	public static void main(String[] args) {
+			 Scanner sc=new Scanner(System.in);
+			 System.out.println("Enter the side: ");
+			 int side=sc.nextInt();
+			 
+			 int area = side*side ;
+			 int perimeter=4*side;
+			 System.out.println("area of square " + area);
+			 System.out.println("perimeter of square " + perimeter);
+}
+
+}
 
