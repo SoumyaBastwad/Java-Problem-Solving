0a283373f9c2e1e7d41b2b924086c794865fed8a
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-01 22:31:23 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,1 +1,20 @@
+package Demo;
+import java.util.Scanner;
+public class Rectangle {
+
+	public static void main(String[] args) {
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter the length: ");
+	 int length=sc.nextInt();
+	 System.out.println("Enter the width: ");
+	 int width=sc.nextInt();
+	 
+	 int area=length+width;
+	 int perimeter=2*(length+width);
+	 System.out.println("area of rectangle " + area);
+	 System.out.println("perimeter of rectangle " + perimeter);
+
+	}
+
+}
 
