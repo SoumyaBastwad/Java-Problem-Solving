918c3296c7eaa57c2d7bffeb21df440d5c5e6ac6
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-01 21:14:02 +0530

    Update Revercesrting.java

--- Revercesrting.java
+++ Revercesrting.java
@@ -6,11 +6,4 @@ public class Revercesrting {
          Scanner sc =new Scanner(System.in);
          System.out.print("Enter the string:");
          String str =sc.nextLine();
-         String reverse ="";
-         for(int i=str.length()-1;i>=0;i--) {
-        	 reverse = reverse  + str.charAt(i);
-         }
-         System.out.println("Reverce string is:" + reverse);
-	}
-}
-        
+           
