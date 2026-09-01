441f8a431a77ae2b8e76879d18a2ef821918aeda
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-01 21:50:45 +0530

    Update Productofpairs.java

--- Productofpairs.java
+++ Productofpairs.java
@@ -1,1 +1,20 @@
+package Demo;
+import java.util.Scanner;
+public class Productofpairs {
+
+	public static void main(String[] args) {
+			    Scanner	sc=new Scanner(System.in);
+		           int a=sc.nextInt();
+		           int b=sc.nextInt();
+		           int c=sc.nextInt();
+		           int prod1=a*b;
+		           int prod2=a*c;
+		           int prod3=b*c;
+		           System.out.println(prod1);
+		           System.out.println(prod2);
+		           System.out.println(prod3);
+
+	}
+
+}
 
