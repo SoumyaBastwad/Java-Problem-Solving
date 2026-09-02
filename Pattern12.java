5ff9952af90382dfd95f480e0561879681c717c7
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-02 22:32:09 +0530

    Update Pattern12.java

--- Pattern12.java
+++ Pattern12.java
@@ -6,15 +6,4 @@ public class Pattern12 {
 		int n=5;
 	   for(int i=1;i<=n;i++) {
 		   for(int k=1;k<=n-i;k++) {
-		    	System.out.print(" ");
-		    }
-		   for(int j=1;j<=i;j++) {
-			   System.out.print("* ");
-		   }
-		   System.out.println();
-	   }
-
-	}
-
-}
 
