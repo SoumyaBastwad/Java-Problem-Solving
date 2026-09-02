f0690631d62f436e9879874f4ea8e772e62dc419
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-02 22:33:04 +0530

    Update Pattern13.java

--- Pattern13.java
+++ Pattern13.java
@@ -1,24 +1,2 @@
-package Demo;
 
-public class Pattern13 {
-
-	public static void main(String[] args) {
-		int n=5;
-		   for(int i=1;i<=n;i++) {
-			   for(int k=1;k<=n-i;k++) {
-			    	System.out.print(" ");
-			    }
-			   for(int j=1;j<=i;j++) {
-				   if(j==1||j==i||i==n) {
-				   System.out.print("* ");
-				   }
-				   else {
-					   System.out.print("  "); 
-				   }
-			   }
-			   System.out.println();
-
-	}
-	}
-}
 
