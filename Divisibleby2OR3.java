a1d895ec644684e5ded9f1c9995ee7b6dd79e3c3
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-03 23:13:04 +0530

    Update Divisibleby2OR3.java

--- Divisibleby2OR3.java
+++ Divisibleby2OR3.java
@@ -1,1 +1,22 @@
+package Demo;
 
+import java.util.Scanner;
+
+public class Divisibleby2OR3 {
+
+	public static void main(String[] args) {
+		Scanner	sc= new  Scanner(System.in);
+		System.out.print("Enter the number");
+		int n=sc.nextInt();
+		
+		if(n%2==0||n%3==0) {
+			System.out.print("yes");
+			
+		}
+		else {
+			System.out.print("no");
+		}
+
+	}
+
+}
