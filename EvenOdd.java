f5c838d875794e370fe7ba3ddac338240022f694
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-03 23:00:00 +0530

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -2,21 +2,15 @@ package Demo;
 import java.util.Scanner;
 
 public class EvenOdd {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 	    Scanner	sc= new  Scanner(System.in);
 		System.out.print("Enter the number");
 		int n=sc.nextInt();
-		
 		if(n%2==0) {
 			System.out.print("even number");
-			
-		}
+			}
 		else {
 			System.out.print("Odd number");
 		}
-
-	}
-
 }
-
+}
