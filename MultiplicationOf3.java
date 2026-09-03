9e522549c9a454d56a404c148f156dd342d914ea
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-03 23:07:48 +0530

    Update MultiplicationOf3.java

--- MultiplicationOf3.java
+++ MultiplicationOf3.java
@@ -1,1 +1,24 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class MultiplicationOf3 {
+
+	public static void main(String[] args) {
+		Scanner	sc= new  Scanner(System.in);
+		System.out.print("Enter the number");
+		int n=sc.nextInt();
+		
+		if(n%3==0) {
+			System.out.print("Multiplication of 3");
+			
+		}
+		else {
+			System.out.print("not Multiplication of 3");
+		}
+
+
+	}
+
+}
 
