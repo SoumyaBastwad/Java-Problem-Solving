4ea7f43d1bf89d19cf43f56ae65a95f1577d39b5
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-03 22:43:44 +0530

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,2 +1,20 @@
+package Demo;
+import java.util.Scanner;
+public class Triangle {
 
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the height: ");
+		int height=sc.nextInt();
+		System.out.println("Enter the base: ");
+		int base=sc.nextInt();
+		System.out.println("Enter the side: ");
+		int side=sc.nextInt();
+	    float area=0.5f*(base)*(height);
+        float perimeter=side+base+side;
+        System.out.println("area of triangle :" +area);
+        System.out.println("perimeter of triangle :" +perimeter);
+	}
+
+}
 	
