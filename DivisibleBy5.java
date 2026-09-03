d2a564c5847a410cc025dda7a8c40de0069ceac1
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-03 23:02:27 +0530

    Update DivisibleBy5.java

--- DivisibleBy5.java
+++ DivisibleBy5.java
@@ -1,1 +1,23 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class DivisibleBy5 {
+
+	public static void main(String[] args) {
+		Scanner	sc= new  Scanner(System.in);
+		System.out.print("Enter the number");
+		int n=sc.nextInt();
+		
+		if(n%5==0) {
+			System.out.print("Divisible by 5");
+			
+		}
+		else {
+			System.out.print(" Not Divisible by 5");
+		}
+
+	}
+
+}
 
