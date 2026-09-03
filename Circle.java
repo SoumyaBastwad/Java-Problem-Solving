89ccc2639b5a134cb4a6b804f97bc7b22bb27645
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-03 22:47:34 +0530

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -3,16 +3,4 @@ import java.util.Scanner;
 
 public class Circle {
 
-	public static void main(String[] args) {
-	  Scanner sc=new Scanner(System.in);
-	  System.out.println("Enter the radius of circle:" );
-	  float radius = sc.nextFloat();
-	  float area = 3.142f*radius*radius;
-	  float perimeter=2*3.142f*radius;
-	  System.out.println("Area of circle: " +area);
-	  System.out.println("perimeter of circle: " +perimeter);
-
-	}
-
-}
-
+	
