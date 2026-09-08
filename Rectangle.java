8aed1e697df1c74a2260c220f7dd7eb65b18ca09
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-08 23:09:59 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,8 +1,7 @@
 package Demo;
 import java.util.Scanner;
 public class Rectangle {
-
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 	 Scanner sc=new Scanner(System.in);
 	 System.out.println("Enter the length: ");
 	 int length=sc.nextInt();
