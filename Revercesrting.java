ea2e326ebcbb4950122411cfe4a4f306920e60e7
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-08 23:11:28 +0530

    Update Revercesrting.java

--- Revercesrting.java
+++ Revercesrting.java
@@ -1,8 +1,7 @@
 package startproject;
 import java.util.Scanner;
 public class Revercesrting {
-
-	public static void main(String[] args) {
+       public static void main(String[] args) {
          Scanner sc =new Scanner(System.in);
          System.out.print("Enter the string:");
          String str =sc.nextLine();
