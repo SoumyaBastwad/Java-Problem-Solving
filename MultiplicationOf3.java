8ee96de9f72f56fb0e52e154dec911da7ed6fa7a
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-08 23:08:25 +0530

    Update MultiplicationOf3.java

--- MultiplicationOf3.java
+++ MultiplicationOf3.java
@@ -1,10 +1,8 @@
 package Demo;
 
 import java.util.Scanner;
-
 public class MultiplicationOf3 {
-
-	public static void main(String[] args) {
+      public static void main(String[] args) {
 		Scanner	sc= new  Scanner(System.in);
 		System.out.print("Enter the number");
 		int n=sc.nextInt();
