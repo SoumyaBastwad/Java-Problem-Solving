c76c6b39c4662a2ba4d46a138844a1d211191de7
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-08 23:05:49 +0530

    Update Divisible2and3and7.java

--- Divisible2and3and7.java
+++ Divisible2and3and7.java
@@ -1,23 +1,18 @@
 package Demo;
 
 import java.util.Scanner;
-
 public class Divisibleby2and3and7 {
-
-	public static void main(String[] args) {
+     public static void main(String[] args) {
 		Scanner	sc= new  Scanner(System.in);
 		System.out.print("Enter the number");
 		int n=sc.nextInt();
-		
 		if(n%2==0 && n%3==0 && n%7==0) {
 			System.out.print("yes");
-			
-		}
+			}
 		else {
 			System.out.print("no");
 		}
-
-	}
+   }
 
 }
 
