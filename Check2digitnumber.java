8506fe242759619abb116c068a967d9560d0d2cf
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-09 21:55:22 +0530

    Update Check2digitnumber.java

--- Check2digitnumber.java
+++ Check2digitnumber.java
@@ -1,1 +1,20 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class Check2digitnumber {
+
+	public static void main(String[] args) {
+		 Scanner 	sc=new Scanner(System.in);
+	        int n =sc.nextInt();
+	        if(9 <n && n>100) {
+	        	System.out.print("yes");
+	        }
+	        else {
+	        	System.out.print("no");
+	        }
+
+	}
+
+}
 
