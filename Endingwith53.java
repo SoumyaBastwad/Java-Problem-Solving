1427c3fe8b42d098cd09cac084fcefb47dfe2252
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-09 22:15:47 +0530

    Update Endingwith53.java

--- Endingwith53.java
+++ Endingwith53.java
@@ -1,10 +1,7 @@
 package Demo;
-
 import java.util.Scanner;
-
 public class Endingwith53 {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		 Scanner 	sc=new Scanner(System.in);
 	        int n =sc.nextInt();
 	        if(n%100==53) {
