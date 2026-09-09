8938f98882c915e65eec2659f76b0d6f27fe8fc6
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-09 22:11:52 +0530

    Update Endingwith0.java

--- Endingwith0.java
+++ Endingwith0.java
@@ -1,8 +1,7 @@
 package Demo;
 import java.util.Scanner;
 public class Endingwith0 {
-
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 	   Scanner 	sc=new Scanner(System.in);
         int n =sc.nextInt();
         if(n%10==0) {
