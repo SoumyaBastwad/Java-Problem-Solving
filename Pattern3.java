448efb63d5a63ffbb4e37f3767170e827f473440
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-10 22:43:14 +0530

    Update Pattern3.java

--- Pattern3.java
+++ Pattern3.java
@@ -1,8 +1,6 @@
 package Demo;
-
 public class Pattern3 {
-
-		public static void main(String[] args) {
+     public static void main(String[] args) {
 		    int i, j;
 		    for(i=1;i<=5;i++) {
 		    	for(j=1;j<=5;j++) {
