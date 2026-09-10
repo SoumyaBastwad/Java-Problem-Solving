1343d78f51e908d4e3f4e7314260eb8e7673ddd7
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-10 22:42:05 +0530

    Update Pattern12.java

--- Pattern12.java
+++ Pattern12.java
@@ -1,8 +1,6 @@
 package Demo;
-
 public class Pattern12 {
-
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 		int n=5;
 	   for(int i=1;i<=n;i++) {
 		   for(int k=1;k<=n-i;k++) {
