43d4dd2b982ac474c8b8807e2022e989f9471235
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-10 22:40:32 +0530

    Update Endingwith7.java

--- Endingwith7.java
+++ Endingwith7.java
@@ -2,7 +2,7 @@ package Demo;
 import java.util.Scanner;
 public class Endingwith7 {
     public static void main(String[] args) {
-		 Scanner 	sc=new Scanner(System.in);
+		 Scanner sc=new Scanner(System.in);
 	        int n =sc.nextInt();
 	        if(n%10==7) {
 	        	System.out.print("yes");
