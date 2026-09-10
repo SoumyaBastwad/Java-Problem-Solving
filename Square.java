a155adef1284d5fdabf090c0dffd0bb588335f21
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-10 22:44:51 +0530

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,9 +1,7 @@
 package Demo;
 import java.util.Scanner;
-
 public class Square {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 			 Scanner sc=new Scanner(System.in);
 			 System.out.println("Enter the side: ");
 			 int side=sc.nextInt();
