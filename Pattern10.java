4f1bcbe939f04b867384deab3764d862463991e2
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-12 22:40:53 +0530

    Update Pattern10.java

--- Pattern10.java
+++ Pattern10.java
@@ -1,7 +1,10 @@
 package Demo;
+import java.util.Scanner;
 public class Pattern10 {
    public static void main(String[] args) {
-		for(int i=1;i<=5;i++) 
+	   Scanner sc=new Scanner(System.in);
+	   int n =sc.nextInt();
+		for(int i=1;i<=n;i++) 
 		{
 		  for(int j=1;j<=i;j++) 
 		  {
