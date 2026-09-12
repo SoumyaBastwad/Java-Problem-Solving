e44d6d10ac674f2d7d409c36da6323be22f86ecd
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-12 22:50:54 +0530

    Update Pattern6.java

--- Pattern6.java
+++ Pattern6.java
@@ -1,10 +1,11 @@
 package Demo;
 import java.util.Scanner;
 public class Pattern6 {
-
-	public static void main(String[] args) {
-		    int i, j;
-		    for(i=1;i<=5;i++) {
+     public static void main(String[] args) {
+		 Scanner sc=new Scanner(System.in);
+		 int n=sc.nextInt();
+		 int i, j;
+		    for(i=1;i<=n;i++) {
 		    	for(j=1;j<=i;j++) {
 		    	    System.out.print("* ");
 		    	}
