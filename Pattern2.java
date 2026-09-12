b3059e9a150c1dca14833e1e81ee39a95675b769
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-12 22:46:58 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -2,9 +2,11 @@ package Demo;
 import java,util.Scanner;
 public class Pattern2 {
     public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
 	    int i, j;
-	    for(i=1;i<=5;i++) {
-	    	for(j=1;j<=5;j++) {
+	    for(i=1;i<=n;i++) {
+	    	for(j=1;j<=n;j++) {
 	    	    System.out.print("* ");
 	    	}
 	    	System.out.println();
