5ee9713882c4101e648b32c5e1f5f48183ef0412
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-12 22:48:36 +0530

    Update Pattern3.java

--- Pattern3.java
+++ Pattern3.java
@@ -1,9 +1,11 @@
 package Demo;
 public class Pattern3 {
      public static void main(String[] args) {
-		    int i, j;
-		    for(i=1;i<=5;i++) {
-		    	for(j=1;j<=5;j++) {
+		 Scanner sc=new Scanner(System.in);
+		 int n = sc.nextInt();
+		 int i, j;
+		    for(i=1;i<=n;i++) {
+		    	for(j=1;j<=n;j++) {
 		    	    System.out.print(i+" ");
 		    	}
 		    	System.out.println();
