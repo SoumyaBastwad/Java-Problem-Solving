6904380ec72c2d8e46ad040c5f95147e7631592a
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-13 22:33:35 +0530

    Update Pattern4.java

--- Pattern4.java
+++ Pattern4.java
@@ -1,9 +1,12 @@
 package Demo;
+import java.util.Scanner;
 public class Pattern4{
 	public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in);
+		int n =sc.nextInt();
 			    int i, j;
-			    for(i=1;i<=5;i++) {
-			    	for(j=1;j<=5;j++) {
+			    for(i=1;i<=n;i++) {
+			    	for(j=1;j<=n;j++) {
 			    	    System.out.print(i +" " );
 			    	}
 			    	System.out.println();
