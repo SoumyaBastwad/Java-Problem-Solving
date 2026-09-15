b0f6d04ec90832e754254de2f7908f1b779347f8
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-15 23:14:11 +0530

    Update PrintUnicodeCharacter.java

--- PrintUnicodeCharacter.java
+++ PrintUnicodeCharacter.java
@@ -1,1 +1,13 @@
+package Demo;
+import java.util.Scanner;
+public class PrintUnicodeCharacter {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int n =sc.nextInt();
+		System.out.print((char)n);
+
+	}
+
+}
 
