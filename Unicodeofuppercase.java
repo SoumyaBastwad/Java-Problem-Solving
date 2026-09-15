b12436c3204b00cba104f55bb3615e89b03d0ac7
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-15 23:16:39 +0530

    Update Unicodeofuppercase.java

--- Unicodeofuppercase.java
+++ Unicodeofuppercase.java
@@ -1,1 +1,20 @@
+package Demo;
+import java.util.Scanner;
+public class Unicodeofuppercase {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		if(n>=65 && n<=90) {
+			System.out.print("yes");
+			
+		}
+		else {
+			System.out.print("no");
+		}
+
+	}
+
+}
 
