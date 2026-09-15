99200c7225efe3f97f1745386c8f7dec02d288bc
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-15 23:15:20 +0530

    Update Removelastdigit.java

--- Removelastdigit.java
+++ Removelastdigit.java
@@ -1,1 +1,15 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class Removelastdigit {
+
+	public static void main(String[] args) {
+		 Scanner 	sc=new Scanner(System.in);
+	        int n =sc.nextInt();
+	        
+	      System.out.print(n/10);
+	}
+
+}
 
