cc7bf85774f8d1260f260f911d9e6fc87cb6cbb8
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-16 22:21:56 +0530

    Update Forloop.java

--- Forloop.java
+++ Forloop.java
@@ -1,1 +1,15 @@
+package Demo;
+import java.util.Scanner;
+public class Forloop {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int n =sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
 
