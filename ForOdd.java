b8990a0eac8b5ccf94583a93acff5c89a4df3087
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-16 22:44:22 +0530

    Update ForOdd.java

--- ForOdd.java
+++ ForOdd.java
@@ -1,1 +1,16 @@
+package Demo;
+import java.util.Scanner;
+public class ForOdd {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        for (int i=1;i<=n;i++) {
+        	if(i%2!=0) {
+        		System.out.println(i);
+        	}
+        }
+	}
+
+}
 
