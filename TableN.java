c6821d27ea43e69f5d2067426264cf5a191117a5
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-16 23:18:16 +0530

    Update TableN.java

--- TableN.java
+++ TableN.java
@@ -1,1 +1,16 @@
+package Demo;
+import java.util.Scanner;
+public class TableN {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int n=sc.nextInt();
+		for( int i=1;i<=10;i++) {
+			int res=n*i;
+			System.out.println(n + "*"+ i+ "=" +res );
+		}
+
+	}
+
+}
 
