943c36bd22cff3430e6e00115889340b41b642e9
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-17 22:18:02 +0530

    Update CheckPrime.java

--- CheckPrime.java
+++ CheckPrime.java
@@ -1,1 +1,26 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        for (int i=1;i<=n;i++) {
+        	if(n%i==0) {
+        		count++;
+        	}
+        }
+        if(count==2) {
+        System.out.print("prime");
+        }
+        else {
+        	System.out.print("not prime");
+        }
+	
+	}
+
+}
 
