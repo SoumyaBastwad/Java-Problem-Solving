0df67af19396e040900eb370c769ce71ee986b35
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-17 21:53:03 +0530

    Update FactorsOfn.java

--- FactorsOfn.java
+++ FactorsOfn.java
@@ -1,1 +1,16 @@
+package Demo;
+import java.util.Scanner;
+public class FactorsOfn {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i=1;i<=n;i++) {
+        	if(n%i==0) {
+        		System.out.println(i);
+        	}
+        }
+	}
+
+}
 
