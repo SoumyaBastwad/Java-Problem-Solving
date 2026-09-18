1332c15edff1758a20d580dedcd7786664b8bebd
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-18 21:58:42 +0530

    Update Checkprime2.java

--- Checkprime2.java
+++ Checkprime2.java
@@ -1,10 +1,7 @@
 package Demo;
-
 import java.util.Scanner;
-
 public class Checkprime2 {
-
-	public static void main(String[] args) {
+     public static void main(String[] args) {
 				Scanner sc = new Scanner(System.in);
 		        int n=sc.nextInt();
 		        int count=0;
