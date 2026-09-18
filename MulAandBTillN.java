b6720bb42090544eebf34d5af7aaa040bba24c01
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-18 21:59:30 +0530

    Update MulAandBTillN.java

--- MulAandBTillN.java
+++ MulAandBTillN.java
@@ -1,8 +1,7 @@
 package Demo;
 import java.util.Scanner;
 public class MulAandBTillN {
-
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
         int n =sc.nextInt();
         int a=sc.nextInt();
