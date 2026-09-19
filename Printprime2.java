d2a6e6dc1e7e265abd81d8df7745eea01c316716
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-19 22:49:01 +0530

    Update Printprime2.java

--- Printprime2.java
+++ Printprime2.java
@@ -1,1 +1,37 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class Printprime2 {
+
+	
+		public static void Printprime(int n){
+			int count=0;
+			for ( int i=2;count<n;i++) {
+				if (Checkprime(i)) {
+					System.out.println(i);
+					count++;
+				}
+			}
+			
+			}
+		 public static boolean Checkprime(int n){
+			for ( int i=2;i<=n/2;i++) {
+	        	if(n%i==0) {
+	        	   	return false;
+	        	}
+	        	}
+			return true;
+		 }
+
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			
+			int n=sc.nextInt();
+	        Printprime(n);
+		}
+
+	
+
+}
 
