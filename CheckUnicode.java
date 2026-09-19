93b39a7a6a9eff52df5f82ca51d96650eaf114ba
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-19 22:53:51 +0530

    Update CheckUnicode.java

--- CheckUnicode.java
+++ CheckUnicode.java
@@ -4,11 +4,4 @@ public class CheckUnicode {
 
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
-		int n =sc.nextInt();
-		if(n>=65 && n<=90) {
-			System.out.print("UpperCase");
-		}else if(n>=97 && n<=122) {
-			System.out.print("LowerCase");
-		}else if(n>=48 && n<=57) {
-			System.out.print("Numeric");
-	
+		
