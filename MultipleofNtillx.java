916465ee1b573c56e01d83df88d16e2563c8d675
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-20 22:58:27 +0530

    Update MultipleofNtillx.java

--- MultipleofNtillx.java
+++ MultipleofNtillx.java
@@ -1,1 +1,17 @@
+package Demo;
+import java.util.Scanner;
+public class MultipleofNtillx {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 int x=sc.nextInt();
+        int n=sc.nextInt();
+        for(int i=1;i<=x;i++) {
+        	if(i%n==0) {
+        		System.out.println(i);
+        	}
+        }
+	}
+
+}
 
