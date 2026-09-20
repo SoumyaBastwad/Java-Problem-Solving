ee46b4f4ce4b7541859c9a34fe33ba385299f438
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-20 23:03:10 +0530

    Update MULLaandbTilln.java

--- MULLaandbTilln.java
+++ MULLaandbTilln.java
@@ -1,1 +1,18 @@
+package Demo;
+import java.util.Scanner;
+public class MULLaandbTilln {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 int n=sc.nextInt();
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       for(int i=1;i<=n;i++) {
+    	   if(i%a==0 && i%b==0) {
+    		   System.out.println(i);
+    	   }
+       }
+	}
+
+}
 
