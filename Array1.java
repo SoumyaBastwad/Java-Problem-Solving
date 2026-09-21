72e08fb5edea1d4254606e344355b59e7c0d87d2
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-21 23:10:07 +0530

    Update Array1.java

--- Array1.java
+++ Array1.java
@@ -1,1 +1,21 @@
+package Demo;
+import java.util.Scanner;
+public class Array1 {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int[] age=new int[5];
+		for(int i=0;i<5;i++) {
+			System.out.println("Enter the studen age:"+ (i+1) );
+			age[i]=sc.nextInt();
+			
+		}
+		System.out.println("Enter the studen ages are:" );
+		for(int i=0;i<5;i++) {
+			System.out.print(age[i] + " ");
+			
+		}
+	}
+
+}
 
