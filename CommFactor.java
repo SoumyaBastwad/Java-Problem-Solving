9bd046c02b3103691fc1338f095a1cc51b78eb3f
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-22 23:44:39 +0530

    Update CommFactor.java

--- CommFactor.java
+++ CommFactor.java
@@ -1,16 +1,3 @@
 package Demo;
 import java.util.Scanner;
-public class CommFactor {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-     int a=sc.nextInt();
-     int b=sc.nextInt();
-     for(int i=1;i<a;i++) {
-  	   if(a%i==0 && b%i==0) {
-  		   System.out.println(i);
-  	   }
-     }
-  }
-}
-
+public class Com
