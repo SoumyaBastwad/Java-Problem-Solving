6f005904fee96c27d5efb21881084bd1824283d6
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-23 21:27:32 +0530

    Update Forloop2.java

--- Forloop2.java
+++ Forloop2.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Forloop2 {
 
 	public static void main(String[] args) {
-		
-				Scanner sc =new Scanner(System.in);
+		Scanner sc =new Scanner(System.in);
 				int n =sc.nextInt();
 				for(int i=n;i>=1;i--) {
 					System.out.println(i);
