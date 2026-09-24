fa2dab8948753c1d75a4d693dfa79b9232c5b0d4
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-24 17:14:37 +0530

    Update Absulutevalue.java

--- Absulutevalue.java
+++ Absulutevalue.java
@@ -1,16 +1,1 @@
-package Demo;
-import java.util.Scanner;
-public class Absulutevalue {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		
-		if(n>=0) {
-			System.out.print(n);
-			
-		}
-		else {
-			System.out.print(-n);
-		}
-	}
-}
+
